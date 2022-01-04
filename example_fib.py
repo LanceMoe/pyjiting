ae1a5c5dad32fcc15aee41d586bce0c7da581233
Lance Moe <admin@lance.moe>
2022-01-04 18:37:17 +0900

    feat: fib example timeit

--- example_fib.py
+++ example_fib.py
@@ -1,9 +1,30 @@
 from pyjiting import jit
+import time
+
 
 @jit
-def fib(x):
+def fib_jit(x):
     if x < 3:
         return 1
-    return fib(x-1) + fib(x-2)
+    return fib_jit(x-1) + fib_jit(x-2)
 
-print(fib(40))
+
+def fib_nojit(x):
+    if x < 3:
+        return 1
+    return fib_nojit(x-1) + fib_nojit(x-2)
+
+
+fib_jit(0)
+fib_nojit(0)
+
+
+start_time = time.time()
+result = fib_jit(40)
+cost_time_ms = (time.time() - start_time) * 1000
+print('fib_jit(40) =', result, f'(cost time: {cost_time_ms} ms)')
+
+start_time = time.time()
+result = fib_nojit(40)
+cost_time_ms = (time.time() - start_time) * 1000
+print('fib_nojit(40) =', result, f'(cost time: {cost_time_ms} ms)')
